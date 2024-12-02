06c076cd034518d7fa1e53c6feb7c28f6c7a859d
Egor Marvelous <marvelchenko@gmail.com>
2024-12-02 07:05:48 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,41 +2,9 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.jpg";
 
-export const HERO_CONTENT = `I am a passionate Frontend Developer and Graphic Designer with over 2 years of experience, eager to explore and grow in the tech industry. I have honed my skills in front-end technologies such as HTML, CSS, React, Node.js, MySQL, Toad for Oracle, Photoshop and CorelDraw, and I am driven to continue developing innovative, high-quality web applications.`;
-
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
-
-export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
+export const HERO_CONTENT = `I am a Frontend Engineer who is passionate about designing and developing user-friendly web interfaces. Proficiency in React.js, TailwindCSS, CSS, and JavaScript, I have gained experience through personal projects and internship at Rivers State ICT Department. I’m eager to enhance my skills continuously and contribute effectively in a collaborative environment.`;
 
 export const PROJECTS = [
   {
@@ -67,12 +35,19 @@ export const PROJECTS = [
     image: project4,
     description:
       "The Expense Tracker is a React application that helps users manage their income and expenses",
-    technologies: ["React", "Context API", "Tailwild CSS"],
+    technologies: ["Vite", "Context API", "Tailwild CSS"],
+    visitLink: "https://marv-expense-tracker.vercel.app/",
+    sourceLink: "https://github.com/marvelchenko/expenseTracker"
+  },
+  {
+    title: "Music Player",
+    image: project5,
+    description:
+      "Built a Music Player App with React.js, CSS and Bootstrap to help users play songs, the app is responsive both in mobile and desktop view, you can switch songs from previous to next",
+    technologies: ["React", "CSS", "Bootstrap 5"],
+    visitLink: "https://music-player-ten-teal.vercel.app/",
+    sourceLink: "https://github.com/marvelchenko/music-player"
   },
 ];
 
-export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
-};
+
